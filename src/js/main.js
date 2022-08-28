e98976462edd9ad3f79254b0e7d09e3ef6814a50
Alexander Grygoriev <truedevelop@yahoo.com>
2022-08-28 20:16:09 +0300

    create video player on click button and stop when close btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import Slider from "./modules/Slider";
+import VideoPlayer from "./modules/PlayVideo";
 
 window.addEventListener('DOMContentLoaded', ()=> {
   const slider = new Slider('.page', '.next');
   slider.render();
+  const player = new VideoPlayer('.showup .play', '.overlay' );
+  player.init();
 });
